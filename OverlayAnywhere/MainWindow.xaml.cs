a64063ac22468c984101bee800624ffbbca896f8
baconcow <baconcowa@gmail.com>
2020-12-01 21:03:29 +0900

    Bug fix

---
+++ OverlayAnywhere/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            this.Topmost = true;
+            this.Top = 0;
+            this.Left = 0;
+            vc.Address = vc_url.Text;
+            text.Address = txt_url.Text;
+            if (vc_url.Text != "")
+                vc_section.Visibility = Visibility.Visible;
+            if (txt_url.Text != "")
+                txt_section.Visibility = Visibility.Visible;
+            initScreen.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void initScreen_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LeftCtrl || e.Key == Key.RightCtrl)
+            {
+                Console.WriteLine("okok");
+                dragControl_vc.Opacity = 0.5;
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            dragControl_vc.Opacity = 0;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Overlay Anywhere
+
+A discord overlay that can be used anywhere on your Windows desktop.
+
+## Download
+
+[Download from here](https://github.com/baconcowa/Overlay-Anywhere/releases/tag/v1.0)
+
+## Usage
+
+#### If you will use the TXT overlay
+
+1. Go to https://streamkit.discord.com/overlay, click on Install for OBS 
+2. Customize your Chat Widget
+3. Copy the link in the left panel after that
+4. Start Overlay Anywhere
+5. Paste it into the second field
+
+#### If you will use the Voice Chat overlay
+
+1. Go to https://streamkit.discord.com/overlay, click on Install for OBS 
+2. Customize your Voice Widget
+   * Notice that due to the limitation of Discord StreamKit, you may have to re-enter the Voice Channel you have selected in Discord manually after you complete the setup of Overlay Anywhere if you enabled "Show Speaking Users Only"
+3. Copy the link in the left panel after that
+4. Go back to Overlay Anywhere
+5. Paste it into the first field
+6. Click Continue and enjoy!
+
+
+
+Just close the program directly from your task bar or in anyway you like if you don't need the overlay anymore.
+
+##### Sure! You can use both together!
+
+#### Moving overlays
+
+##### Moving TXT overlay
+
+* Press and hold you mouse on top of the TXT overlay and start dragging around
+
+##### Moving VC overlay
+
+- Switch and focus on the window of Overlay Anywhere you can do this by clicking on it in the task bar
+- Hold Ctrl on keyboard and you will see a black translucent box appeared
+- Press and hold you mouse on top of it and start dragging around
+- Release Ctrl after you are done dragging
+
+##### 
